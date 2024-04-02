54d846722c4293c9685c9efc830507d74a123d46
Diana Kosyanenko <kosyanenkodiana@yandex.ru>
2024-04-02 23:33:57 +0300

    new rep

--- 4 (3).js
+++ 4 (3).js
@@ -12,12 +12,9 @@ xxxx
 xxxxx
 */
 for (let x = 0; x <= 20; x++) {
-    let str = "";
-    for (let y = 0; y <= x; y++) {
-        str += "*"
-        
-    }
-    console.log(str);
-    
+  let str = "";
+  for (let y = 0; y <= x; y++) {
+    str += "*";
+  }
+  console.log(str);
 }
-
